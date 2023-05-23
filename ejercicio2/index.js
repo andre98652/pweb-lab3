0938c1bbfcb8066f8ab369fa709b826b57c85b4c
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-05-23 15:07:18 -0500

    ejercicio2

---
+++ ejercicio2/index.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Escuchando en: http://localhost:3000")
+    console.log("preuba")
+
+});
+
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'index.html'));
+});
